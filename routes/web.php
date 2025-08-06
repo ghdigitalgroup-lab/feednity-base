da37dd1b51942377b104676ffd02a01c52749928
ghdigitalgroup-lab <ghdigitalgroup@gmail.com>
2025-08-07 00:17:30 +0200

    Add Jetstream Livewire and Cashier setup

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,13 @@ Route::get('/', function () {
 
 Route::get('/onboarding', [OnboardingController::class, 'create']);
 Route::post('/onboarding', [OnboardingController::class, 'store']);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
